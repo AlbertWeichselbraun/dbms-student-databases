7a3baa975095b64e017cf4ead1d847461b44f284
Albert Weichselbraun <albert@weichselbraun.net>
2021-09-08 10:30:54 +0200

    chg: directly run commands.

--- generate-dbs.py
+++ generate-dbs.py
@@ -1,30 +1,46 @@
 #!/usr/bin/env python3
+"""
+Setup student databases and permissions
+"""
+
+import subprocess
+from glob import glob
 
 DATABASES = ('projektdatenbank', 'unidb', 'world', 'bundesliga', 'wetterdaten')
+USER_FILE_PATHS = '/home/*/dbms_users.txt'
 
-for users in glob('/home/*/dbms_users.txt'):
+
+cmd = []
+for users in glob(USER_FILE_PATHS):
     with open("users.txt") as f:
-        print("createdb -E UTF8 -O postgres test")
+
+        # create the test database
+        cmd.append('createdb -E UTF8 -O postgres test')
+
+        # create the exam databases
         for db in DATABASES:
-            print("createdb -E UTF8 -O postgres {}".format(db))
-            print("zcat /root/databases/{}.sql.gz |psql {}".format(db, db))
-    
+            cmd.append(f'createdb -E UTF8 -O postgres {db}')
+            cmd.append(f'zcat /root/databases/{db}.sql.gz |psql {db}')
+
         for user in f:
             if not user.strip() or user.startswith("#"):
                 continue
-    
+
             user_name = user.split("@")[0]
             user_pass = user.split(".")[0].capitalize() + "7000"
-            
-            print("\n# " + user.strip())
-            print("# ------------------------------")
-            print("createuser {}".format(user_name))
-            print("echo \"ALTER USER \\\"{}\\\" PASSWORD '{}'\" | psql postgres".format(user_name, user_pass))
-            for db in DATABASES + ('test', ): 
+
+            cmd.append(f'\n# {user.strip()}')
+            cmd.append('# ------------------------------')
+            cmd.append(f"createuser {user_name}")
+            cmd.append(f"echo \"ALTER USER \\\"{user_name}\\\" PASSWORD '{user_pass}'\" | psql postgres")
+            for db in DATABASES + ('test', ):
                 # access to the default database
-                print("echo \"GRANT CONNECT ON DATABASE {} TO \\\"{}\\\";\"|psql {}".format(db, user_name, db))
-                print("echo \"GRANT USAGE ON SCHEMA public TO \\\"{}\\\";\"|psql {}".format(user_name, db))
-    
-                db_name = "{}.{}".format(db, user_name)
-                print("echo \"DROP DATABASE IF EXISTS \\\"{}\\\"; CREATE DATABASE \\\"{}\\\" WITH TEMPLATE {} OWNER \\\"{}\\\"; GRANT ALL PRIVILEGES ON DATABASE \\\"{}\\\" TO \\\"{}\\\"\" | psql postgres".format(db_name, db_name, db, user_name, db_name, user_name))
-                print("echo \"GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO \\\"{}\\\"\" | psql {}".format(user_name, db_name))
+                cmd.append(f"echo \"GRANT CONNECT ON DATABASE {db} TO \\\"{user_name}\\\";\"|psql {db}")
+                cmd.append(f"echo \"GRANT USAGE ON SCHEMA public TO \\\"{user_name}\\\";\"|psql {db}")
+
+                db_name = f"{db}.{user_name}"
+                cmd.append(f"echo \"DROP DATABASE IF EXISTS \\\"{db_name}\\\"; CREATE DATABASE \\\"{db_name}\\\" WITH TEMPLATE {db} OWNER \\\"{user_name}\\\"; GRANT ALL PRIVILEGES ON DATABASE \\\"{db_name}\\\" TO \\\"{user_name}\\\"\" | psql postgres")
+                cmd.append(f"echo \"GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO \\\"{user_name}\\\"\" | psql {db_name}")
+
+# execute the database commands
+subprocess.run(['/bin/bash'], input='\n'.join(cmd).encode('utf-8'))
